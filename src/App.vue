6c9f4ef2644b9f9912a18dd11b131c951a98d94b
WaltGurley <walt.gurley@gmail.com>
2017-07-28 15:07:31 -0400

    iniitial commit

---
+++ src/App.vue
@@ -0,0 +1,120 @@
+<template>
+  <div id="app">
+    <div class="search-fields">
+      <h1>Bunch-O-Kitties</h1>
+      <h2>GIF stress</h2>
+      <!-- <input type="text" v-model="searchTerm" placeholder="Search Term"> -->
+      <!-- <button type="button" name="button" v-on:click="searchGif">Search</button> -->
+    </div>
+    <gif-holder v-bind:gifSubject="searchResponse"></gif-holder>
+    <div class="test-wrapper"></div>
+  </div>
+</template>
+
+<script>
+import GifHolder from './components/GifHolder'
+
+const key = '28a10c5337904e8fa4f600bda2469814'
+
+export default {
+  name: 'app',
+  components: {
+    GifHolder
+  },
+  data () {
+    return {
+      searchTerm: '',
+      searchResponse: []
+    }
+  },
+  methods: {
+    searchGif () {
+      fetch(
+        'https://api.giphy.com/v1/gifs/search?api_key=' + key +
+        '&q=' + this.searchTerm + '&limit=100&offset=5&rating=G&lang=en'
+      )
+      .then(response => response.json())
+      .then(data => { this.searchResponse = data.data })
+    }
+  },
+  mounted () {
+    fetch(
+      'https://api.giphy.com/v1/gifs/search?api_key=' + key +
+      '&q=kittens&limit=100&offset=5&rating=G&lang=en'
+    )
+    .then(response => response.json())
+    .then(data => { this.searchResponse = data.data })
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #fad1d1;
+  background-color: #222;
+}
+
+$immersion-width: 6816px;
+$immersion-height: 2240px;
+
+$tile-width: 426px;
+$tile-height: 320px;
+
+$grid-columns: 16;
+$grid-rows: 7;
+
+.search-fields {
+  height: 100%;
+  width: $tile-width * 3;
+  font-size: 5vh;
+
+  input, button {
+    font-size: 5vh;
+  }
+}
+
+.test-wrapper {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: $immersion-width;
+  height: $immersion-height;
+  z-index: 666;
+
+  pointer-events: none;
+
+  .transparent-image-overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: $immersion-width;
+    height: $immersion-height;
+    background-image: image-url("immersion/immersion-test.jpg");
+    opacity: 0.25;
+  }
+
+  .tile {
+    box-sizing: border-box;
+    border-color: #000;
+    border-width: 1px;
+    border-style: solid;
+    background-color: none;
+  }
+
+  @for $i from 1 through ($grid-columns * $grid-rows) {
+    .tile-#{$i - 1} {
+      width: $tile-width;
+      height: $tile-height;
+      float: left;
+    }
+  }
+}
+</style>
---
+++ src/components/GifHolder.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="gif-holder">
+    <div v-for="gif in gifSubject" class="img-holder">
+      <img v-bind:src="gif.images.original.url" alt="" class="gif-img">
+    </div>
+    <!-- {{ gifSubject[0].images.fixed_height.url }} -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'gif-holder',
+  props: ['gifSubject'],
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  $immersion-width: 6816px;
+  $immersion-height: 2240px;
+
+  $tile-width: 426px;
+  $tile-height: 320px;
+
+  $grid-columns: 16;
+  $grid-rows: 7;
+
+  .gif-holder {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    align-items: flex-start;
+    width: $tile-width * 13;
+    height: 100%;
+
+    .img-holder {
+      height: $tile-height;
+
+      .gif-img {
+        height: 100%;
+      }
+    }
+  }
+</style>
